3144c80ae299579a5019cb2637767b4b46921c40
Davide <malagoli@gmail.com>
2013-03-11 22:43:23 +0100

    removed index

--- base/app/models/post.rb
+++ base/app/models/post.rb
@@ -4,9 +4,9 @@ class Post < ActiveRecord::Base
   alias_attribute :text, :description
   validates_presence_of :text
 
-  define_index do
-    activity_object_index
-  end
+  #define_index do
+  #  activity_object_index
+  #end
 
   def title
     description.truncate(30, :separator =>' ')
